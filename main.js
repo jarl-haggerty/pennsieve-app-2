7a459f3e6394935f86117db572b7705af2cb6f54
Jarl Haggerty <Jarl.Haggerty@pennmedicine.upenn.edu>
2024-07-30 14:14:20 -0400

    Add mime types for static content

--- main.js
+++ main.js
@@ -10,20 +10,45 @@ const server = http.createServer((req, res) => {
   console.log('getting ' + filename);
   fs.readFile(filename, (err, data) => {
     if(err) {
+      const json = JSON.stringify(err);
+      console.log('error ' + filename + ' ' + json);
       res.writeHead(404);
-      res.end(JSON.stringify(err));
+      res.end(json);
       return;
     }
     const headers = {};
     if(filename.endsWith('.html')) {
       headers['Content-Type'] = 'text/html';
+    } else if(filename.endsWith('.xml')) {
+      headers['Content-Type'] = 'application/xml';
     } else if(filename.endsWith('.css')) {
       headers['Content-Type'] = 'text/css';
     } else if(filename.endsWith('.js')) {
       headers['Content-Type'] = 'text/javascript';
     } else if(filename.endsWith('.json')) {
       headers['Content-Type'] = 'application/json';
+    } else if(filename.endsWith('.svg')) {
+      headers['Content-Type'] = 'image/svg+xml';
+    } else if(filename.endsWith('.svg')) {
+      headers['Content-Type'] = 'image/svg+xml';
+    } else if(filename.endsWith('.png')) {
+      headers['Content-Type'] = 'image/png';
+    } else if(filename.endsWith('.jpg')) {
+      headers['Content-Type'] = 'image/jpeg';
+    } else if(filename.endsWith('.txt')) {
+      headers['Content-Type'] = 'text/plain';
+    } else if(filename.endsWith('.woff')) {
+      headers['Content-Type'] = 'font/woff';
+    } else if(filename.endsWith('.woff2')) {
+      headers['Content-Type'] = 'font/woff2';
+    } else if(filename.endsWith('.ttf')) {
+      headers['Content-Type'] = 'font/ttf';
+    } else if(filename.endsWith('.eot')) {
+      headers['Content-Type'] = 'application/vnd.ms-fontobject';
+    } else if(filename.endsWith('.ico')) {
+      headers['Content-Type'] = 'image/vnd.microsoft.icon';
     }
+    console.log('got ' + filename + ' ' + headers['Content-Type']);
     res.writeHead(200, headers);
     res.end(data);
   });
